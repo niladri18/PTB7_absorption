03b5839650f3bccc48e9755b00480bd3b45f92ea
niladri18 <nldrgomeser48@gmail.com>
2017-04-24 12:51:30 -0700

    modified file sci.f90 where HF is called as a subroutine

--- src/sci.f90
+++ src/sci.f90
@@ -2,6 +2,7 @@ program ppv
 
         use parameter
         use readfile
+        use hartree
 
 !open(unit=1,file="dipole_n.inp")
 open(unit=10,file="./output/nonlinear.out")
@@ -83,27 +84,6 @@ allocate(ilbond(2,200))
 
         call read_config
  
- !read(91,*) n
-! read(91,*) nbonds1,nbonds2,nbonds3,nbonds4!,nbonds5 
-! nbnd=nbonds1+nbonds2+nbonds3+nbonds4!+nbonds5
-! print*,"total no of bonds",nbnd
- !nbonds1 = C-S; nbonds2 = C-C; nbonds3 = C=C; nbonds4 = benzene bonds; nbonds5 = interchain bonds
-! read(91,*) t1,t2,t3,tb,kappa,eta
- !write(*,*) 't1,t2,t3',t1,t2,t3
- !Define this array variable: ilbnd(2,100)
-! read(91,*) (ilbond(1,k),ilbond(2,k),k=1,nbnd)
- !write(*,*) 'links',(ilbond(1,k),ilbond(2,k),k=1,nbnd)
- !Define siten, Hub-U, and chem-pot arrays
-! read(91,*) (eps(i),i=1,n)
-! read(91,*) (U_c(i),i=1,n)
-! read(91,*) (z(i),i=1,n)
- !write(*,*) 'z',(z(i),i=1,n)
-
-
-! print*,"HOMO",n/2+sulp/2,"LUMO",n/2+sulp/2+1
-! do i=1,n
-! print*,x_pos(i),z(i),U(i)
-! end do
  
 
 ! print*,"ended reading coordinates"
@@ -249,111 +229,12 @@ end do
 end do
 
 
-        ! end calculating the coulomb interactions
+! end calculating the coulomb interactions
         
  print*,"Hartree-Fock calculation starts"   
 
-do hf=1,400
 
-!! self consistent calculation starts
-        it=it+1
-
-
-        ! calculating charge density at site i
- do i=1,n
-        charge(i)=0.0
-        !p_on(i)=0.0
-        do k=1,n/2+sulp/2
-                charge(i)=charge(i)+2.0*v_n(i,k)*v_n(i,k)
-        end do
-end do
-f=0.0
-
-! Assigning the matrix elements of the many body hamiltonian
-
-
-
- 
-
-
-!!  Assiging off-diagonal elements
-
-	 do i=1,n
-
-	 do j=1,n
-
-	
-	if(i.ne.j)then
-	
-		p_off=0.0
-		do k=1,n/2+sulp/2
- 			p_off=p_off+2.0*v_n(i,k)*v_n(j,k)
- 			!print*,i,j,v_n(i,k)*v_n(j,k)
- 		end do
-		f(i,j)=f0(i,j)-0.5*p_off*v_ij(i,j)
-		f(j,i)=f(i,j)
-		
-	end if
-	
-	end do
-	end do
-		
-	
-
-
-		
- 	!! end of assigning the off diagonal elements
- 	
- 	!! Assigning the diagonal elements
- 	
- 	
- 	
- 	
- 	
- 	
- 	do i=1,n
- 	p_on(i)=0.0
- 	do j=1,n
- 		if(i.ne.j)then
- 		p_on(i)=p_on(i) + (charge(j)-z(j))*v_ij(i,j)
- 		!print*,"Chemical pot",i,j,z(j),charge(j),v_ij(i,j)
- 		end if
- 	end do
- 	f(i,i)=p_on(i)+0.5*charge(i)*U_c(i)+eps(i)
- 	end do
- 	
- 	!!!! end assigning the diagonal elements
- 	
-	
-
-
-	
-	call DSYEV('V','U', N, f , N, tmp, WORK, lwork, info )
-
-
- 	
- 	
- 	err=0.0
- 	do i=1,n
- 	
- 	err1=(tmp(i)-d_n(i))**2
- 	err=err+err1
- 	!print*,i,j,v_ij(i,j),v_ij(j,i)
- 	end do
- 	conv=sqrt(err)/n
- 	
-
-	v_n =f
- 	d_n=tmp
-
-
-
-
-
-
-
- if(conv.le.0.0000001)exit
-end do
+call hartree_fock
 
  !call eigsrt(d_hf,v_hf)
 
